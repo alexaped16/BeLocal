c6f7c902ccd16f505320f956f9cf7c1711bbeca7
Alexa Ped <alexaped16@gmail.com>
2022-05-04 16:09:51 -0500

    adding modules

--- app/models.py
+++ app/models.py
@@ -6,9 +6,7 @@ from flask_login import UserMixin
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
+
 
 
 @login.user_loader
